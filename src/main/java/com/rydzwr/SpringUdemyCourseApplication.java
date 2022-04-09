0782a45c97ef317f7649532afa6b4ac8837b8bb8
rydzwr <rydzwr@gmail.com>
2022-04-09 12:04:55 +0200

    BUILD: Project

---
+++ src/main/java/com/rydzwr/SpringUdemyCourseApplication.java
@@ -0,0 +1,13 @@
+package com.rydzwr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUdemyCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUdemyCourseApplication.class, args);
+	}
+
+}

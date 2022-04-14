402da3b21942f84da037f6477a0b4f9372b24e06
rydzwr <rydzwr@gmail.com>
2022-04-14 19:16:10 +0200

    BUILD: Task & TaskRepo with desc validation

---
+++ src/main/java/com/rydzwr/model/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.rydzwr.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+interface TaskRepository extends JpaRepository<Task, Integer>
+{
+
+}
